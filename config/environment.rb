6819483458534e75baa72ebf13fbd155de654184
Bakhtawar Maqsood <bakhtawar.maqsood@devsinc.com>
2021-09-22 13:36:37 +0500

    changes in config

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@
 
 # Load the Rails application.
 require_relative 'application'
+# Initialize the Rails application.
+
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     config.action_mailer.delivery_method = :smtp
@@ -14,8 +16,7 @@ Rails.application.configure do
       enable_starttls_auto: true }
   end
 
-# Initialize the Rails application.
-Rails.application.initialize!
+  Rails.application.initialize!
 
 PUBLISHABLE_KEY="pk_test_51JZWMuEVSKB0mGfOY5sTEpVGMlvmxq6LuOkWzaGZEzrDTkCatKblEo4ZogeVlR5S5iHku4USfSgMn3jw7Tmi4Kl700dUvrravD"
 SECRET_KEY="sk_test_51JZWMuEVSKB0mGfOILroTaaY9Vu5wmtfxxSEAIqkzt65XvhhILv7ebRCW41VJxm4yL2lzbJiy5KBH3NpaGc7T1fX00DDmXFSy0"
