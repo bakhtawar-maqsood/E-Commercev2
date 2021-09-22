f94b8258efeaf83af6ab3cba12b18bdb98d95efb
Bakhtawar Maqsood <bakhtawar.maqsood@devsinc.com>
2021-09-22 13:07:46 +0500

    environment set

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,15 @@
 
 # Load the Rails application.
 require_relative 'application'
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'bakhtawar.maqsood@devsinc.com',
+    password:             'dcbjugyqvyliuwvz',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 
 # Initialize the Rails application.
 Rails.application.initialize!
