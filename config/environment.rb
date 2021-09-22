9d343a174414bbc88c203559aeac8d263db82935
Bakhtawar Maqsood <bakhtawar.maqsood@devsinc.com>
2021-09-22 13:29:45 +0500

    errorfix

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,17 @@
 
 # Load the Rails application.
 require_relative 'application'
+Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      user_name:            'bakhtawar.maqsood@devsinc.com',
+      password:             'dcbjugyqvyliuwvz',
+      authentication:       'plain',
+      enable_starttls_auto: true }
+  end
 
 # Initialize the Rails application.
 Rails.application.initialize!
