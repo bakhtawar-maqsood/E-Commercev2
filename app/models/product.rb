980b0687639df8e24177ccf56978c32bac4e4aef
Bakhtawar Maqsood <bakhtawar.maqsood@devsinc.com>
2021-09-23 13:02:30 +0500

    Views Indentation and Front end Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   belongs_to :user
 
   has_many :comments, dependent: :destroy
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
 
   has_and_belongs_to_many :wishlists
@@ -14,7 +14,7 @@ class Product < ApplicationRecord
   has_many_attached :images
 
   validates :name, :price, presence: true
-  validate :image_type
+  validate :image_type_validation
 
   after_update :email_to_wisher
 
@@ -36,12 +36,18 @@ class Product < ApplicationRecord
     images[input].variant(resize: '200x200!').processed
   end
 
-  def image_type
+  def image_type_validation
     errors.add(:images, 'are missing') if images.attached? == false
     images.each do |image|
       errors.add(:images, 'needs to be a JPEG or PNG') unless image.content_type.in?(%(image/jpeg image/png))
     end
   end
+
+  # def image_size_validation
+  #   images.each do |image|
+  #     errors.add(:images, 'should be less than 2MB') if image.size_range > 2.megabytes
+  #   end
+  # end
 
   def email_to_wisher
     if :price_changed?
