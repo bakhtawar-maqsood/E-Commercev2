433bd3610b11f9b0d4f7f8bbee02a6675bdc6d59
Bakhtawar Maqsood <bakhtawar.maqsood@devsinc.com>
2021-09-24 11:53:41 +0500

    Added Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'ffaker'
-5.times do
-  t = User.create(name: FFaker::Name.name, email: FFaker::Internet.unique.email, cnic: "25202-2246788-9", phone_no: "0320-6666666", address: FFaker::Address.street_address, password: "123456")
-  3.times do |i|
-    puts t.id
-    product = Product.new(name: FFaker::Name.name,serial_no: "pkk-00#{i}", user_id: t.id)
-    product.images.attach(io: File.open('/'), filename: "logo.png",content_type: 'image/png')
+5.times do |i|
+  @t = User.create(name: FFaker::Name.name, email: FFaker::Internet.unique.email, cnic: "25202-2246788-9", phone_no: "0320-6666666", address: FFaker::Address.street_address, password: "123456")
+  3.times do |j|
+    @product = Product.new(name: FFaker::Name.name, serial_no: "pk-00#{i}000#{j}", price: 1000, user_id: @t.id)
+    @product.images.attach(io: File.open('app/assets/images/logo.png'), filename: "logo.png",content_type: 'image/png')
+    @product.save
   end
 end
